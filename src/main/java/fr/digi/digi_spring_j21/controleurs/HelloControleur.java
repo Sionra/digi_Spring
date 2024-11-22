6ffae1783d9e022b5d3fe35fcd4de59ad2c2eb35
Sionra <farfadet.31@hotmail.fr>
2024-11-22 12:23:26 +0100

    TP2

--- src/main/java/fr/digi/digi_spring_j21/controleurs/HelloControleur.java
+++ src/main/java/fr/digi/digi_spring_j21/controleurs/HelloControleur.java
@@ -1,5 +1,6 @@
 package fr.digi.digi_spring_j21.controleurs;
 
+import fr.digi.digi_spring_j21.services.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
+    HelloService helloService = new HelloService();
 
     @GetMapping
     public String sayHello() {
-        return "Hello";
+        return helloService.salutation();
     }
 }
